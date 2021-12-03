7604c19c7fb485aa844e07d94e38d0204d024282
Om Mishra <om.mishra@intel.com>
2021-12-03 15:15:50 +0530

    this commit for sortted array

---
+++ sorted-array.c
@@ -0,0 +1,31 @@
+int  main()
+{
+    int i,j,n,a[100],temp;
+ 
+    printf("Enter the number of elements:\n") ;
+    scanf("%d",&n) ;
+ 
+    printf("Enter the elements\n") ;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]) ;
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("Elements sorted in ascending order are\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]) ;
+    }
+}
